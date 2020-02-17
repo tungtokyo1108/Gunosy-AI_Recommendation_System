cc0e0eed2356d687061fe45010bd586abe79f415
TungUTokyo <dangthanhtung91@gmail.com>
2020-02-17 20:46:56 +0900

    Add recommendation system

--- backend/server/apps/ml/Gunosy_classifier/NaiveBayes.py
+++ backend/server/apps/ml/Gunosy_classifier/NaiveBayes.py
@@ -15,6 +15,10 @@ import requests
 import bs4 
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import MeCab
+from gensim.models import word2vec
+from gensim.models import Doc2Vec
+from gensim.models.doc2vec import TaggedDocument
+from tqdm import tqdm, tqdm_pandas, tqdm_notebook
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -25,16 +29,12 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.utils.extmath import safe_sparse_dot
 from sklearn.utils.fixes import logsumexp
-
-from collections import defaultdict
-import re
-import collections
+from sklearn.metrics.pairwise import cosine_similarity
 
 import warnings
 warnings.filterwarnings("ignore")
 
 class NaiveBayes:
-    
     def __init__(self, alpha=0.01):
         path_to_artifacts = "../../research/"
         self.alpha = alpha
@@ -71,7 +71,7 @@ class NaiveBayes:
         news = news[cols]
     
         return news
-    """ 
+        
     def preprocessing(self, input_data):
         
         df = input_data.reset_index(drop=True)
@@ -90,21 +90,35 @@ class NaiveBayes:
             wakati_text_list.append(get_wakati_text(text))
 
         df["wakati_text"] = wakati_text_list
+        self.df_pred = df
         
         with open("News_dataset.pickle", "rb") as data:
-            df_train = pickle.load(data)
-            df_train = df_train.reset_index(drop=True).drop(columns = ["News_length"])
+            self.df_train = pickle.load(data)
+            self.df_train = self.df_train.reset_index(drop=True).drop(columns = ["News_length"])
 
-        df_train["Content_Parsed_1"] = df_train["Article"].str.replace("キーワードで気になるニュースを絞りこもう 「いいね」、フォローをしておすすめの記事をチェックしよう。 グノシーについて 公式SNS 関連サイト アプリをダウンロード グノシー | 情報を世界中の人に最適に届ける Copyright © Gunosy Inc. All rights reserved.", '')
-        nrows = len(df_train)
+        with open("Updated_news.pickle", "rb") as data:
+            self.df_pre_recommend = pickle.load(data)
+            self.df_pre_recommend = self.df_pre_recommend.reset_index(drop=True).drop(columns = ["News_length"])
+            
+        self.df_train["Content_Parsed_1"] = self.df_train["Article"].str.replace("キーワードで気になるニュースを絞りこもう 「いいね」、フォローをしておすすめの記事をチェックしよう。 グノシーについて 公式SNS 関連サイト アプリをダウンロード グノシー | 情報を世界中の人に最適に届ける Copyright © Gunosy Inc. All rights reserved.", '')
+        nrows = len(self.df_train)
         wakati_text_list = []
         for row in range(0, nrows):
-            text = df_train.loc[row]["Content_Parsed_1"]
+            text = self.df_train.loc[row]["Content_Parsed_1"]
             wakati_text_list.append(get_wakati_text(text))
 
-        df_train["wakati_text"] = wakati_text_list
+        self.df_train["wakati_text"] = wakati_text_list
+
+        self.df_pre_recommend["Content_Parsed_1"] = self.df_pre_recommend["Article"].str.replace("キーワードで気になるニュースを絞りこもう 「いいね」、フォローをしておすすめの記事をチェックしよう。 グノシーについて 公式SNS 関連サイト アプリをダウンロード グノシー | 情報を世界中の人に最適に届ける Copyright © Gunosy Inc. All rights reserved.", '')
+        nrows = len(self.df_pre_recommend)
+        wakati_text_list = []
+        for row in range(0, nrows):
+            text = self.df_pre_recommend.loc[row]["Content_Parsed_1"]
+            wakati_text_list.append(get_wakati_text(text))
+
+        self.df_pre_recommend["wakati_text"] = wakati_text_list
         
-        #df = pd.concat([df, df_train]).reset_index(drop=True)
+        df = pd.concat([df, self.df_train]).reset_index(drop=True)
         
         vectorizer = TfidfVectorizer(use_idf = True, token_pattern=u'(?u)\\b\\w+\\b')
         X = vectorizer.fit_transform(df.wakati_text.values)
@@ -122,11 +136,9 @@ class NaiveBayes:
                              if x == "国内" else 6
                              if x == "IT・科学" else 7)
         
-        return df_train["Content_Parsed_1"], y, df["Content_Parsed_1"]
-    """
+        return X, y, X_pred
     
     """
-    In this current version, I have tried to run Naive Bayes based on some funtions below. 
     Reference: 
         https://nlp.stanford.edu/IR-book/html/htmledition/naive-bayes-text-classification-1.html
     """
@@ -208,105 +220,68 @@ class NaiveBayes:
         predict_log_prob = jll - np.atleast_2d(log_prob_x).T
         predict_prob = np.exp(predict_log_prob)
         
-        return predict_prob
-    
-    """
-    In this current version, I have tried to run Naive Bayes from scratch. 
-    Reference: 
-        https://github.com/joelgrus/data-science-from-scratch/blob/master/scratch/naive_bayes.py
-    """
-    
-    def preprocessing(self, input_data):
-        
-        df = input_data.reset_index(drop=True)
-        df["Content_Parsed_1"] = df["Article"].str.replace("キーワードで気になるニュースを絞りこもう 「いいね」、フォローをしておすすめの記事をチェックしよう。 グノシーについて 公式SNS 関連サイト アプリをダウンロード グノシー | 情報を世界中の人に最適に届ける Copyright © Gunosy Inc. All rights reserved.", '')
-        
-        with open("News_dataset.pickle", "rb") as data:
-            df_train = pickle.load(data)
-            df_train = df_train.reset_index(drop=True).drop(columns = ["News_length"])
+        return predict_prob, predict
 
-        df_train["Content_Parsed_1"] = df_train["Article"].str.replace("キーワードで気になるニュースを絞りこもう 「いいね」、フォローをしておすすめの記事をチェックしよう。 グノシーについて 公式SNS 関連サイト アプリをダウンロード グノシー | 情報を世界中の人に最適に届ける Copyright © Gunosy Inc. All rights reserved.", '')
+    def recommend(self, input_data):
+
+        if input_data == 0:
+            label = "エンタメ"
+        elif input_data == 1:
+            label = "スポーツ"
+        elif input_data == 2:
+            label = "グルメ"
+        elif input_data == 3:
+            label = "海外"
+        elif input_data == 4:
+            label = "おもしろ"
+        elif input_data == 5:
+            label = "国内"
+        elif input_data == 6:
+            label = "IT・科学"
+        else:
+            label = "コラム"
+
+        self.df_pred["Category"] = label
+
+        df_pred_recommed = self.df_pred[["Category", "Title", "PageLink", "wakati_text"]]
+        df_train_recommed = self.df_pre_recommend[["Category", "Title", "PageLink", "wakati_text"]]
+
+        df_recommend = pd.concat([df_pred_recommed, df_train_recommed]).reset_index(drop=True)
         
-        y = df_train["Category"].apply(lambda x: 0 
-                             if x == "エンタメ" else 1 
-                             if x == "スポーツ" else 2
-                             if x == "グルメ" else 3
-                             if x == "海外" else 4 
-                             if x == "おもしろ" else 5
-                             if x == "国内" else 6
-                             if x == "IT・科学" else 7)
+        def get_doc_mean_vector(doc, model):
+            doc_vector = np.zeros(model.vector_size)
+            words = doc.split()
+            word_cnt = 0 
+            for word in words:
+                try:
+                    word_vector = model.wv[word]
+                    doc_vector += word_vector 
+                    word_cnt += 1
+                except KeyError:
+                    pass
+            doc_vector /= word_cnt 
+            return doc_vector
         
-        return df_train["Content_Parsed_1"], y, df["Content_Parsed_1"]
+        news_recommend = df_recommend[df_recommend.Category == label].reset_index(drop=True)
+        #news_inter = news_inter.iloc[[0,1]]
+        corpus_recommend = [doc.split() for doc in news_recommend.wakati_text.values]
+        model_recommend = word2vec.Word2Vec(corpus_recommend, size=1000, min_count=20, window=10)
+        X_recommend = np.zeros((len(news_recommend), model_recommend.wv.vector_size))
+        for i, doc in tqdm_notebook(enumerate(news_recommend.wakati_text.values)):
+            X_recommend[i, :] = get_doc_mean_vector(doc, model_recommend)
+
+        similar = np.zeros(len(X_recommend))
+        for i in range(0, len(X_recommend)):
+            #similar.append(cosine_similarity(X_inter[0].reshape(1, -1), X_inter[i].reshape(1, -1)))
+            similar[i] = cosine_similarity(X_recommend[0].reshape(1, -1), X_recommend[i].reshape(1, -1))
     
-    def dict_words(self, example, dict_index):
-        for token_word in example:
-            self.bow_dicts[dict_index][token_word] += 1
-            
-    def tokenize(self, text):
-        tagger = MeCab.Tagger("-Owakati")
-        wakati_text = tagger.parse(text).strip().split()
-        return wakati_text
-    
-    def train(self, dataset, labels):
-        self.input_data = dataset
-        self.labels = labels
-        self.classese = np.unique(labels)
-        self.bow_dicts = np.array([defaultdict(lambda: 0) for index in range(self.classese.shape[0])])
-        
-        for cat_index, cat in enumerate(self.classese): 
-            all_cat_text = self.input_data[self.labels==cat]
-            cleaned_text = [self.tokenize(cat_example) for cat_example in all_cat_text]
-            cleaned_text = pd.DataFrame(data=cleaned_text)
-            np.apply_along_axis(self.dict_words, 1, cleaned_text, cat_index)
-        
-        prob_classes = np.empty(self.classese.shape[0])
-        all_words = []
-        cat_word_counts = np.empty(self.classese.shape[0])
-        for cat_index, cat in enumerate(self.classese):
-            prob_classes[cat_index] = np.sum(self.labels==cat)/float(self.labels.shape[0])
-            cat_word_counts[cat_index] = np.sum(np.array(list(self.bow_dicts[cat_index].values()))) + 1
-            all_words += self.bow_dicts[cat_index].keys()
-        
-        uniqueWords = collections.Counter()
-        for word in all_words:
-            uniqueWords[word] += 1
-        self.vocab = uniqueWords.keys()
-        self.vocab_length = len(self.vocab)
-        
-        denoms = np.array([cat_word_counts[cat_index] + self.vocab_length+1 for cat_index, cat in enumerate(self.classese)])
-        self.cats_info = [(self.bow_dicts[cat_index], prob_classes[cat_index], denoms[cat_index]) for cat_index, cat in enumerate(self.classese)]
-        self.cats_info = np.array(self.cats_info)
-        
-    
-    def joint_log_likelihood(self, test_example):
-        
-        likelihood_prob = np.zeros(self.classese.shape[0])
-        
-        for cat_index, cat in enumerate(self.classese):
-            for test_token in test_example:
-                test_token_counts = self.cats_info[cat_index][0].get(test_token, 0) + 1
-                test_token_prob = test_token_counts/float(self.cats_info[cat_index][2])
-                likelihood_prob[cat_index] += np.log(test_token_prob)
-                
-        post_prob = np.empty(self.classese.shape[0])
-        for cat_index, cat in enumerate(self.classese):
-            post_prob[cat_index] = likelihood_prob[cat_index] + np.log(self.cats_info[cat_index][1])
-            
-        return post_prob
-    
-    def predict(self, test_set):
-        predictions = []
-        predict_prob = []
-        for example in test_set:
-            cleaned_example = self.tokenize(example)
-            post_prob = self.joint_log_likelihood(cleaned_example)
-            predictions.append(self.classese[np.argmax(post_prob)])
-            
-            log_prob_x = logsumexp(post_prob)
-            predict_log_prob = post_prob - np.atleast_2d(log_prob_x).T
-            predict_prob.append(np.exp(predict_log_prob))
-        
-        return np.concatenate(predict_prob, axis=0)
+        df_similar = pd.DataFrame(similar, columns=["Cosine_similarity"])
+        df_recommendation_unsort = pd.concat([news_recommend[['Title', 'PageLink']], df_similar['Cosine_similarity']], axis=1)
+        df_recommendation_unsort = df_recommendation_unsort.sort_values(by=['Cosine_similarity'], ascending=False).reset_index(drop=True)
+        self.df_recommendation = df_recommendation_unsort.drop(df_recommendation_unsort.index[0]).reset_index(drop=True)
+
+
+        return self.df_recommendation
     
     def postprocessing(self, input_data):
         
@@ -322,6 +297,28 @@ class NaiveBayes:
                 "Probablity for Group_1st is: ": round(data_pred.loc[0, 'prob']*100,2),
                 "Group_2nd: ": data_pred.loc[1, 'label'],
                 "Probablity for Group_2nd is: ": round(data_pred.loc[1, 'prob']*100,2),
+                "-------------------------------------------------------------------": " ",
+                "Recommendation": "The ranked lists With Title, Link, Level of similarity",
+                "*********": " ",
+                "1st_Title": self.df_recommendation.iloc[0]['Title'],
+                "1st_link": self.df_recommendation.iloc[0]['PageLink'],
+                "1st_Score_similarity": round(self.df_recommendation.iloc[0]['Cosine_similarity']*100, 2),
+                "**********": " ",
+                "2nd_Title": self.df_recommendation.iloc[1]['Title'],
+                "2nd_link": self.df_recommendation.iloc[1]['PageLink'],
+                "2nd_Score_similarity": round(self.df_recommendation.iloc[1]['Cosine_similarity']*100, 2),
+                "***********": " ",
+                "3rd_Title": self.df_recommendation.iloc[2]['Title'],
+                "3rd_link": self.df_recommendation.iloc[2]['PageLink'],
+                "3rd_Score_similarity": round(self.df_recommendation.iloc[2]['Cosine_similarity']*100, 2),
+                "*************": " ",
+                "4th_Title": self.df_recommendation.iloc[3]['Title'],
+                "4th_link": self.df_recommendation.iloc[3]['PageLink'],
+                "4th_Score_similarity": round(self.df_recommendation.iloc[3]['Cosine_similarity']*100, 2),
+                "***************": " ",
+                "5th_Title": self.df_recommendation.iloc[4]['Title'],
+                "5th_link": self.df_recommendation.iloc[4]['PageLink'],
+                "5th_Score_similarity": round(self.df_recommendation.iloc[4]['Cosine_similarity']*100, 2),
                 "status: ": "OK"}
         
     def compute_prediction(self, input_links):
@@ -329,9 +326,8 @@ class NaiveBayes:
             input_data = self.get_news(input_links)
             X, y, X_pred = self.preprocessing(input_data)
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-            self.train(X_train, y_train)
-            prediction = self.predict(X_pred)
-            #prediction = self.estimate_predict(X_train, y_train, X_pred)
+            prediction, group = self.estimate_predict(X_train, y_train, X_pred)
+            recommendation = self.recommend(group)
             prediction = self.postprocessing(prediction)
         except Exception as e:
             return {"status": "Error", "message": str(e)}
@@ -341,36 +337,6 @@ class NaiveBayes:
 
 # Test 
 my_algo = NaiveBayes()
-input_links = "https://gunosy.com/articles/R5g7f"        
+input_links = "https://gunosy.com/articles/RZQor"        
 my_algo.compute_prediction(input_links)
         
-input_data = my_algo.get_news(input_links)
-X, y, X_pred = my_algo.preprocessing(input_data)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-prediction = my_algo.train(X_train, y_train)
-prediction = my_algo.predict(X_pred)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
